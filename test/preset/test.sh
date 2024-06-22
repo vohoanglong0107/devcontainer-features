1a8137be6b447665ac1d0d4c256974843a209070
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-06-22 23:59:49 +0900

    feat(preset): install atuin

--- test/preset/test.sh
+++ test/preset/test.sh
@@ -14,16 +14,16 @@
 #    "remoteUser": "root"
 # }
 #
-# Thus, the value of all options will fall back to the default value in 
+# Thus, the value of all options will fall back to the default value in
 # the Feature's 'devcontainer-feature.json'.
 # For the 'preset' feature, that means the default favorite greeting is 'hey'.
 #
 # These scripts are run as 'root' by default. Although that can be changed
 # with the '--remote-user' flag.
-# 
+#
 # This test can be run with the following command:
 #
-#    devcontainer features test \ 
+#    devcontainer features test \
 #                   --features preset   \
 #                   --remote-user root \
 #                   --skip-scenarios   \
@@ -40,9 +40,15 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "execute nvim" bash -c "nvim --version"
+check "execute atuin" bash -c "atuin --version"
+check "execute bat" bash -c "bat --version"
 check "execute chezmoi apply successfully" bash -c "chezmoi cat ~/.zshrc"
+check "execute difftastic" bash -c "difft --version"
+check "execute fd" bash -c "fd --version"
 check "execute github cli successfully" bash -c "gh"
+check "execute lazygit" bash -c "lazygit --version"
+check "execute nvim" bash -c "nvim --version"
+check "execute ripgrep" bash -c "rg --version"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
